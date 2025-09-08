faea119586ee3f54a25bdf7b7bd64cffbe33299b
harshupocof1 <harshupocof1@gmail.com>
2025-09-08 21:16:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,12 @@ class Config:
     # It's highly recommended to set this as an environment variable in production
     MONGO_URI = (
     os.environ.get('MONGO_URI') or
-    "mongodb+srv://harshdeep_db_user:5aBM8pWh5nXgqOZ4@sih.zkbv6yk.mongodb.net/?retryWrites=true&w=majority&appName=Sih"
+    "mongodb+srv://harshdeep_db_user:5aBM8pWh5nXgqOZ4@sih.zkbv6yk.mongodb.net/sih_db?retryWrites=true&w=majority&appName=Sih"
 )
 
 
     # --- QR Token Config ---
     QR_REFRESH_RATE_SECONDS = 2
     TOKEN_VALIDITY_SECONDS = 5
+
 
