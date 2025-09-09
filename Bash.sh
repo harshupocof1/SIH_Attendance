2bc4ed7e9aa03b544210aa07834b2886c115d635
harshupocof1 <harshupocof1@gmail.com>
2025-09-09 21:21:30 +0530

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
-# Use sudo to get permissions to install system packages
-sudo apt-get update
-sudo apt-get install -y portaudio19-dev
+# Install system packages without sudo
+apt-get update
+apt-get install -y portaudio19-dev
 
 # Install Python dependencies
 pip install -r requirements.txt
