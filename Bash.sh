2f2590fb96056fa86e326777d4ca1d455ca286a4
harshupocof1 <harshupocof1@gmail.com>
2025-09-09 21:09:39 +0530

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -2,4 +2,9 @@
 # exit on error
 set -o errexit
 
+# Install system dependencies for pyaudio
+apt-get update
+apt-get install -y portaudio19-dev
+
+# Install Python dependencies
 pip install -r requirements.txt
